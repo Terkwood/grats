000cbdbc5caac8457f34879e8e31ef90b3388e11
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-08-31 15:42:32 -0400

    write various

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,20 +1,82 @@
+use super::Page;
+use crate::model::*;
+use crate::repo::*;
 use yew::prelude::*;
 
 pub struct App {
-    _link: ComponentLink<Self>,
+    page: Page,
+    gratitude_list_repo: GratitudeListRepo,
+    gratitude_list: GratitudeList,
+
+    entry_buttons_repo: EntryButtonsRepo,
+    entry_buttons: EntryButtonCollection,
+
+    nav_state: NavState,
+}
+
+pub enum Msg {
+    AddEntry(Entry),
+    NavigateTo(Page),
+    ShowNav(bool),
+    AddEntryButton(Emoji),
+    DeleteEntryButton(Emoji),
+}
+
+#[derive(PartialEq)]
+pub enum NavState {
+    Visible,
+    Hidden,
 }
 
 impl Component for App {
-    type Message = ();
-
+    type Message = Msg;
     type Properties = ();
 
-    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        todo!()
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let nav_to = Some(link.callback(|page| Msg::NavigateTo(page)));
+        let show_nav = Some(link.callback(|b| Msg::ShowNav(b)));
+
+        let gratitude_list_repo = GratitudeListRepo::new();
+        let gratitude_list = gratitude_list_repo.read();
+
+        let entry_buttons_repo = EntryButtonsRepo::new();
+        let entry_buttons = entry_buttons_repo.read();
+
+        Self {
+            page: Page::Daily,
+            nav_state: NavState::Visible,
+            gratitude_list_repo,
+            gratitude_list,
+            entry_buttons_repo,
+            entry_buttons,
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        todo!()
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddEntry(entry) => {
+                self.gratitude_list.add(entry);
+                self.gratitude_list_repo.save(&self.gratitude_list)
+            }
+            Msg::NavigateTo(page) => self.page = page,
+            Msg::ShowNav(b) => {
+                if b {
+                    self.nav_state = NavState::Visible
+                } else {
+                    self.nav_state = NavState::Hidden
+                }
+            }
+            Msg::AddEntryButton(emoji) => {
+                self.entry_buttons.add(emoji);
+                self.entry_buttons_repo.save(&self.entry_buttons)
+            }
+            Msg::DeleteEntryButton(emoji) => {
+                self.entry_buttons.delete(&emoji);
+                self.entry_buttons_repo.save(&self.entry_buttons);
+            }
+        }
+
+        true
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
--- src/repo/mod.rs
+++ src/repo/mod.rs
@@ -1,14 +1,15 @@
-use crate::model::GratitudeList;
+use crate::model::*;
 use yew::format::Json;
 use yew::services::storage::{Area, StorageService};
 
-const GRATITUDE_KEY: &str = "grats";
+const GRATITUDE_LIST_KEY: &str = "gratitude_list";
+const ENTRY_BUTTONS_KEY: &str = "entry_buttons";
 
-pub struct GratitudeRepo {
+pub struct GratitudeListRepo {
     pub storage_service: StorageService,
 }
 
-impl GratitudeRepo {
+impl GratitudeListRepo {
     pub fn new() -> Self {
         let storage_service =
             StorageService::new(Area::Local).expect("storage was disabled by the user");
@@ -16,16 +17,42 @@ impl GratitudeRepo {
         Self { storage_service }
     }
 
-    pub fn read_entries(&self) -> GratitudeList {
-        if let Json(Ok(restored_model)) = self.storage_service.restore(GRATITUDE_KEY) {
+    pub fn read(&self) -> GratitudeList {
+        if let Json(Ok(restored_model)) = self.storage_service.restore(GRATITUDE_LIST_KEY) {
             restored_model
         } else {
             GratitudeList::empty()
         }
     }
 
-    pub fn save_entries(&mut self, grat_list: &GratitudeList) {
+    pub fn save(&mut self, grat_list: &GratitudeList) {
         let value = Json(grat_list);
-        self.storage_service.store(GRATITUDE_KEY, value)
+        self.storage_service.store(GRATITUDE_LIST_KEY, value)
+    }
+}
+
+pub struct EntryButtonsRepo {
+    pub storage_service: StorageService,
+}
+
+impl EntryButtonsRepo {
+    pub fn new() -> Self {
+        let storage_service =
+            StorageService::new(Area::Local).expect("storage was disabled by the user");
+
+        Self { storage_service }
+    }
+
+    pub fn read(&self) -> EntryButtonCollection {
+        if let Json(Ok(restored_model)) = self.storage_service.restore(ENTRY_BUTTONS_KEY) {
+            restored_model
+        } else {
+            EntryButtonCollection::empty()
+        }
+    }
+
+    pub fn save(&mut self, buttons: &EntryButtonCollection) {
+        let value = Json(buttons);
+        self.storage_service.store(ENTRY_BUTTONS_KEY, value)
     }
 }
