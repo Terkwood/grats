3cef06cd47d3d5d66bd4151364b0e81f048a79b8
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-05 07:33:48 -0400

    Add tests

--- src/model/history.rs
+++ src/model/history.rs
@@ -35,3 +35,55 @@ impl History {
         Self { days, offset }
     }
 }
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use crate::model::*;
+
+    const LOCAL_OFFSET_SECONDS: i32 = 240;
+
+    fn make_time(days: u32) -> DateTime<Utc> {
+        Utc.ymd(2020, 08, days + 1).and_hms(0, 0, 0)
+    }
+
+    fn list_with(size: u32) -> GratitudeList {
+        let emoji: Emoji = Emoji("🤩".to_string());
+
+        let mut entries = vec![];
+        for i in 0..size {
+            entries.push(Entry {
+                emoji: emoji.clone(),
+                text: "yes".to_string(),
+                time: UtcMillis(make_time(i).timestamp_millis() as u64),
+            });
+        }
+        GratitudeList { entries }
+    }
+    #[test]
+    fn test_grouping() {
+        let size = 10;
+        let inventory = list_with(size);
+        let history = History::from(&inventory, FixedOffset::west(LOCAL_OFFSET_SECONDS));
+
+        assert_eq!(history.days.len(), size as usize);
+        for day in history.days {
+            assert_eq!(day.gratitude_list.entries.len(), 1)
+        }
+        assert_eq!(history.offset, FixedOffset::west(LOCAL_OFFSET_SECONDS))
+    }
+
+    #[test]
+    fn test_from_order() {
+        let size = 3;
+        let inventory = list_with(3);
+        let history = History::from(&inventory, FixedOffset::west(LOCAL_OFFSET_SECONDS));
+
+        for i in 0..(size - 1) {
+            let this_date = history.days[i].date;
+            let next_date = history.days[i + 1].date;
+            assert!(this_date > next_date);
+        }
+    }
+}
