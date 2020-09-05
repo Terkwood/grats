b592251f1cc46d0d7f511548686a85dca09c365a
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-05 05:00:49 -0400

    Use dep

--- src/model/history.rs
+++ src/model/history.rs
@@ -1,4 +1,14 @@
-pub struct History {}
+#[derive(Debug)]
+pub struct History {
+    pub days: Vec<Day>,
+    pub offset: FixedOffset,
+}
+
+#[derive(Ord, Eq, PartialOrd, PartialEq, Debug)]
+pub struct Day {
+    pub date: Date<FixedOffset>,
+    pub inventory: Inventory,
+}
 
 impl History {
     pub fn days(&self) {
