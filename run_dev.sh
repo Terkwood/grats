0392eb46157051019d163e2b2abe7ece6fc06ad2
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-12-30 10:20:39 -0500

    build: update script

--- run_dev.sh
+++ run_dev.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-MYIP=$(ip addr|grep 192|awk '{ print $2 }'|sed 's;\/[0-9]*;;')
-sh build.sh --dev && miniserve --port 8888 -i $MYIP ./dist --index index.html
+sh build.sh --dev && miniserve --port 8888 --interfaces 127.0.0.1 ./dist  --index index.html
